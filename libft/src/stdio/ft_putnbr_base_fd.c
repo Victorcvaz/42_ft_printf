6bd2297644fe2a66aa63b35ff1bbeb44f4b9d632
Victor Cesar Vaz <vcesar-v@c3r3p5.42sp.org.br>
2023-09-30 16:48:17 -0300

    feat: add ft_putnbr_base_fd

--- libft/src/stdio/ft_putnbr_base_fd.c
+++ libft/src/stdio/ft_putnbr_base_fd.c
@@ -6,28 +6,21 @@
 /*   By: vcesar-v <vcesar-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 10:07:22 by victorcvaz        #+#    #+#             */
-/*   Updated: 2023/09/30 16:46:08 by vcesar-v         ###   ########.fr       */
+/*   Updated: 2023/09/28 17:45:55 by vcesar-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/ft_printf.h"
+#include "../../libft.h"
 
-ssize_t	ft_putnbr_base_fd(unsigned long int nbr, char *base, int fd)
+ssize_t	ft_putnbr_base_fd(long int nbr, char *base, int fd)
 {
-	ssize_t	total_bytes;
-	size_t	base_len;
+	char	*nbr_base;
+	ssize_t	bytes;
 
-	if (ft_check_base_errors(base))
-		return (0);
-	base_len = ft_strlen(base);
-	total_bytes = 0;
-	if (nbr < 0)
-	{
-		nbr = -nbr;
-		total_bytes += ft_putchar_fd('-', fd);
-	}
-	if (nbr >= base_len)
-		total_bytes += ft_putnbr_base_fd(nbr / base_len, base, fd);
-	total_bytes += ft_putnbr_base_fd(nbr % base_len, base, fd);
-	return (total_bytes);
+	nbr_base = ft_itoa_base(nbr, base);
+	if (!nbr_base)
+		return (ft_putstr_fd(NULL, fd));
+	bytes = ft_putstr_fd(nbr_base, fd);
+	free(nbr_base);
+	return (bytes);
 }
